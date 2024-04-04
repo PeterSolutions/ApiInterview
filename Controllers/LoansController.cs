f628d92e8d8a89d4b847f023e4c2a352d3efc722
Francisco Ferreira <ferreiradeveloper@gmail.com>
2024-04-04 16:18:37 +0200

    retunr book mejorado

--- Controllers/LoansController.cs
+++ Controllers/LoansController.cs
@@ -1,5 +1,6 @@
 ﻿using DataBase;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiInterview.Controllers
 {
@@ -31,6 +32,26 @@ namespace ApiInterview.Controllers
 
             return Ok("Book loaned successfully to user.");
         }
+
+
+        //Metodo para retornar un book
+        [HttpPost("return/{bookId}")]
+        public IActionResult ReturnBook(int bookId)
+        {
+            var loan = _context.Loans
+                               .Where(l => l.BookID == bookId && l.ReturnDate == null)
+                               .OrderByDescending(l => l.LoanDate)
+                               .FirstOrDefault();
+
+            if (loan != null)
+            {
+                loan.ReturnDate = DateTime.Now;
+                _context.SaveChanges();
+                return Ok("Book returned successfully.");
+            }
+
+            return NotFound("Loan record not found.");
+        }
     }
 
     public class LoanRequest
@@ -38,4 +59,6 @@ namespace ApiInterview.Controllers
         public int UserId { get; set; }
         public int BookId { get; set; }
     }
+
+    
 }
